8c4b372a98a49d56bacb6e77067994f190aa9982
Diego Taioli <de.taioli@itsprodigi.it>
2025-12-02 14:53:03 +0100

    verifica Zeri

--- src/Application.java
+++ src/Application.java
@@ -3,7 +3,25 @@ public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] number = {5, -4, 0, 12, 40, 6};
+		
+		boolean checkZeri = false;
+		for(int i = 0; i< number.length; i++)
+		{
+			if(number[i] == 0)
+			{
+				checkZeri = true;
+			}
+		}
+		
+		if(checkZeri) 
+		{
+			System.out.println("l'array contiene zeri");
+		}
+		else
+		{
+			System.out.println("l'array contiene zeri");
+		}
 	}
 
 }
